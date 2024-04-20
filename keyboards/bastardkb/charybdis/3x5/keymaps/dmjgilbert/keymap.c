f89b685ca4412fea67dcdf68f9cfe35a64342dd7
DMJGilbert <dmjgilbert@me.com>
2024-04-20 22:28:10 +0100

    feat(charybdis): add personal keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/dmjgilbert/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/dmjgilbert/keymap.c
@@ -1,71 +1,85 @@
-/**
- * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
-enum charybdis_keymap_layers {
-    LAYER_BASE = 0,
-    LAYER_LOWER,
-    LAYER_RAISE,
+// Thumb keymaps
+#define LY1_ETR LT(1, KC_ENTER)       // Layer one when held, enter when tapped
+#define LY2_ESC LT(2, KC_ESCAPE)      // Layer two when held, esc when tapped
+#define LT_BSPC MT(MOD_RSFT, KC_BSPC) // Right shift when held, backspace when tapped
+#define LCT_DOT MT(MOD_RCTL, KC_DOT)  // Right control when held, dot when tapped
+
+#define WIN_MOV HYPR(KC_M) //
+#define WIN_RSZ HYPR(KC_R) //
+#define WIN_SWI HYPR(KC_C) //
+
+#define DSP_LFT HYPR(KC_Q) //
+#define DSP_RHT HYPR(KC_W) //
+
+#define WRK_LFT HYPR(KC_A) //
+#define WRK_RHT HYPR(KC_S) //
+
+#define WIN_FUL HYPR(KC_F) //
+
+enum charybdis_keymap_layers { _DAZMAK = 0, _SYMBOL, _NAVIG };
+
+const uint16_t PROGMEM left_mouse_click_combo[]  = {KC_N, KC_E, COMBO_END};
+const uint16_t PROGMEM right_mouse_click_combo[] = {KC_E, KC_I, COMBO_END};
+
+combo_t key_combos[] = {
+    [LEFT_MOUSE_CLICK]  = COMBO_ACTION(left_mouse_click_combo),
+    [RIGHT_MOUSE_CLICK] = COMBO_ACTION(right_mouse_click_combo),
 };
 
-#define LOWER MO(LAYER_LOWER)
-#define RAISE MO(LAYER_RAISE)
-
-#define CTL_BSP CTL_T(KC_BSPC)
-#define SFT_SPC SFT_T(KC_SPC)
-#define GUI_ENT GUI_T(KC_ENT)
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case LEFT_MOUSE_CLICK:
+            if (pressed) {
+                tap_code16(KC_BTN1);
+            }
+            break;
+        case RIGHT_MOUSE_CLICK:
+            if (pressed) {
+                tap_code16(KC_BTN2);
+            }
+            break;
+    }
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         CTL_BSP, SFT_SPC,   LOWER,      RAISE, GUI_ENT
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
+    [_DAZMAK] = LAYOUT(
+        //,--------------------------------------------.                    ,--------------------------------------------.
+            XXXXXXX,    KC_Q,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, XXXXXXX,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+               KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                         KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M,    KC_W, KC_COMM, LCT_DOT,
+        //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                       KC_LGUI, LY1_ETR,  KC_SPC,    LT_BSPC, LY2_ESC
+                                   //`--------------------------'  `--------------------------'
+    ),
 
-  [LAYER_LOWER] = LAYOUT(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       RGB_TOG, KC_MNXT, KC_MPLY, KC_MPRV, XXXXXXX,    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,  QK_BOOT,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
+    [_SYMBOL] = LAYOUT(
+        //,--------------------------------------------.                    ,--------------------------------------------.
+            XXXXXXX, KC_DQUO,KC_QUOTE,  KC_GRV,  KC_DLR,                      KC_CIRC, KC_LBRC,  KC_EQL, KC_RBRC, XXXXXXX,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+            KC_EXLM, KC_QUES, KC_PERC, KC_SCLN,   KC_AT,                      KC_ASTR, KC_LPRN, KC_COLN, KC_RPRN, KC_PLUS,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+            KC_BSLS, KC_TILD, KC_PIPE, KC_HASH, KC_AMPR,                      KC_SLSH, KC_LCBR, KC_UNDS, KC_RCBR, KC_MINS,
+        //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                       KC_LGUI, _______,  KC_SPC,     KC_DEL, _______
+                                   //`--------------------------'  `--------------------------'
+    ),
 
-  [LAYER_RAISE] = LAYOUT(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT, EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         _______, _______, XXXXXXX,    _______, XXXXXXX
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
+    [_NAVIG] = LAYOUT(
+        //,--------------------------------------------.                    ,--------------------------------------------.
+            XXXXXXX, KC_MPLY,  KC_DOT, KC_VOLD, KC_VOLU,                      WIN_MOV, WRK_LFT,   KC_UP, WRK_RHT, XXXXXXX,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+              KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                      WIN_SWI, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGUP,
+        //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+              KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,                      WIN_RSZ, DSP_LFT, WIN_FUL, DSP_RHT, KC_PGDN,
+        //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                       KC_LALT, _______,  KC_TAB,    LT_BSPC, _______
+                                   //`--------------------------'  `--------------------------'
+    ),
 };
+
 // clang-format on
